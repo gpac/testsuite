c5e623ab5e5c65652e209d2de14ee64e51553028
jeanlf <jeanlf@users.sourceforge.net>
2019-11-13 17:29:26 +0100

    fixed hash for gpac/gpac@89cc3a6c9

--- scripts/dash-mux-ondemand.sh
+++ scripts/dash-mux-ondemand.sh
@@ -4,11 +4,14 @@ test_begin "dash-mux-ondemand"
 
 do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_1280x720_512kbps.264 -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_audio.aac -new $TEMP_DIR/file.mp4" "dash-input-preparation"
 
-do_test "$MP4BOX -dash 1000 -profile dashavc264:onDemand $TEMP_DIR/file.mp4 -out $TEMP_DIR/file.mpd" "basic-dash-if-ondemand"
+do_test "$MP4BOX -dash 1000 -profile onDemand $TEMP_DIR/file.mp4 -out $TEMP_DIR/file.mpd" "basic-dash-mux-ondemand"
 
-do_hash_test $TEMP_DIR/file.mpd "mpd"
+#commented on filters as master and filters diverge here on small details - to uncomment once we merge
+#do_hash_test $TEMP_DIR/file.mpd "mpd"
 do_hash_test $TEMP_DIR/file_dashinit.mp4 "seg"
 
-do_playback_test "$TEMP_DIR/file.mpd" "play-dash-if-ondemand"
+myinspect=$TEMP_DIR/inspect.txt
+do_test "$GPAC -i $TEMP_DIR/file.mpd inspect:allp:deep:interleave=false:log=$myinspect"
+do_hash_test $myinspect "inspect"
 
 test_end
