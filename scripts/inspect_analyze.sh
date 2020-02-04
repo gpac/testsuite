66b386d5eebc5ea2530c224770a346d53eb315ea
jeanlf <jeanlf@users.sourceforge.net>
2020-02-04 17:25:29 +0100

    added tmcd analyze test

--- scripts/inspect_analyze.sh
+++ scripts/inspect_analyze.sh
@@ -11,19 +11,19 @@ fi
 
 inspect="$TEMP_DIR/inspect.xml"
 
-do_test "$GPAC -i $1 inspect:deep:log=$inspect:analyze" "inspect"
+do_test "$GPAC -i $1 inspect:deep:log=$inspect:analyze$2" "inspect"
 do_hash_test $inspect "inspect"
 
 test_end
 
 }
 
-test_inspect $MEDIA_DIR/auxiliary_files/count_video.cmp
-test_inspect $MEDIA_DIR/auxiliary_files/count_english.mp3
-test_inspect $MEDIA_DIR/auxiliary_files/enst_video.h264
-test_inspect $MEDIA_DIR/auxiliary_files/counter.hvc
-test_inspect $MEDIA_DIR/auxiliary_files/video.av1
-
+test_inspect $MEDIA_DIR/auxiliary_files/count_video.cmp ""
+test_inspect $MEDIA_DIR/auxiliary_files/count_english.mp3 ""
+test_inspect $MEDIA_DIR/auxiliary_files/enst_video.h264 ""
+test_inspect $MEDIA_DIR/auxiliary_files/counter.hvc ""
+test_inspect $MEDIA_DIR/auxiliary_files/video.av1 ""
+test_inspect $EXTERNAL_MEDIA_DIR/qt_prores/prores422.mov ":SID=#PID=11"
 
 test_begin "inspect-info"
 if [ "$test_skip" != 1 ] ; then
@@ -33,3 +33,4 @@ do_hash_test $inspect "inspect"
 
 fi
 test_end
+
