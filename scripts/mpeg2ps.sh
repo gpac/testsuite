81d49386f393c2651d62a6f1fd64eee2b951f8f1
jeanlf <jeanlf@users.sourceforge.net>
2020-02-12 13:15:31 +0100

    more coverage tests for m2ps

--- scripts/mpeg2ps.sh
+++ scripts/mpeg2ps.sh
@@ -6,8 +6,14 @@ test_begin "m2ps-dmx"
 if [ $test_skip != 1 ] ; then
 insfile=$TEMP_DIR/dump.txt
 #inpect demuxed file in non-interleave mode (pid by pid), also dumps PCR
-do_test "$GPAC -i $srcfile inspect:start=4.0:interleave=false:deep:pcr:log=$insfile" "inspect"
+do_test "$GPAC -i $srcfile inspect:interleave=false:deep:pcr:log=$insfile" "inspect"
 do_hash_test "$insfile" "inspect"
+
+insfile=$TEMP_DIR/dump_from10.txt
+#inpect demuxed file in non-interleave mode (pid by pid), also dumps PCR
+do_test "$GPAC -i $srcfile inspect:start=10.0:interleave=false:deep:pcr:log=$insfile" "inspect"
+do_hash_test "$insfile" "inspect-10"
+
 fi
 test_end
 
