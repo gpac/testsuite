8cfe9a0bd3ba6bd081779704f0c205a4bf542ee9
jeanlf <jeanlf@users.sourceforge.net>
2020-04-17 14:42:48 +0200

    added missing hash

--- scripts/dash-splitset.sh
+++ scripts/dash-splitset.sh
@@ -3,6 +3,7 @@
 test_begin "dash-splitset"
 
 do_test "$GPAC -i $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_1280x720_512kbps.264 -i $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_640x360_192kbps.264 -o $TEMP_DIR/file.mpd:profile=live" "dash"
+do_hash_test $TEMP_DIR/file.mpd "MPD"
 
 myinspect=$TEMP_DIR/inspect.txt
 do_test "$GPAC -i $TEMP_DIR/file.mpd:split_as inspect:allp:deep:interleave=false:dur=2/1:log=$myinspect" "inspect"
