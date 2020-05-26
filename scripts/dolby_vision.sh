c130fc608e93ec6e51c9396619cd5dc2d62efc45
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2020-05-26 20:00:16 +0200

    remove debug trace

--- scripts/dolby_vision.sh
+++ scripts/dolby_vision.sh
@@ -4,7 +4,6 @@ if [ $test_skip = 1 ] ; then
 fi
 
 do_test "$MP4BOX -dash 2000 $MEDIA_DIR/dolby_vision/dolby_vision_cenc.ismv -out $TEMP_DIR/dolby_vision_cenc_dash.mpd" "dashing"
-ls $TEMP_DIR
 do_hash_test $TEMP_DIR/dolby_vision_cenc_dashinit.mp4 "dash-seg"
 do_hash_test $TEMP_DIR/dolby_vision_cenc_dash.mpd "dash-mpd"
 
