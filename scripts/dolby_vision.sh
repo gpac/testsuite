a6f2e9073ee1bb0cb74fd464635c3b9f6d9f45a0
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2020-05-26 18:43:29 +0200

    dolby vision test: misc improvements based on maintainers feedback

--- scripts/dolby_vision.sh
+++ scripts/dolby_vision.sh
@@ -3,8 +3,9 @@ if [ $test_skip = 1 ] ; then
  return
 fi
 
-do_test "$MP4BOX -for-test -dash 2000 1135909523.ismv" 2> /dev/null
-do_hash_test $TEMP_DIR/1135909523_dashinit.mp4 "dump-dash-seg"
-do_hash_test $TEMP_DIR/1135909523_dash.mpd "dump-dash-mpd"
+do_test "$MP4BOX -dash 2000 $MEDIA_DIR/dolby_vision/dolby_vision_cenc.ismv -out $TEMP_DIR/dolby_vision_cenc_dash.mpd" "dashing"
+ls $TEMP_DIR
+do_hash_test $TEMP_DIR/dolby_vision_cenc_dashinit.mp4 "dash-seg"
+do_hash_test $TEMP_DIR/dolby_vision_cenc_dash.mpd "dash-mpd"
 
 test_end
