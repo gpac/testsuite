204281d42c6c43624b6bc7e16bbb51eb2663e7ba
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2020-05-26 20:29:23 +0200

    dolby vision: add DASH test (#3)
    * dolby vision: add DASH test

---
+++ scripts/dolby_vision.sh
@@ -0,0 +1,10 @@
+test_begin "dovi-cenc-dash"
+if [ $test_skip = 1 ] ; then
+ return
+fi
+
+do_test "$MP4BOX -dash 2000 $MEDIA_DIR/dolby_vision/dolby_vision_cenc.ismv -out $TEMP_DIR/dolby_vision_cenc_dash.mpd" "dashing"
+do_hash_test $TEMP_DIR/dolby_vision_cenc_dashinit.mp4 "dash-seg"
+do_hash_test $TEMP_DIR/dolby_vision_cenc_dash.mpd "dash-mpd"
+
+test_end
