5440c70afc266db020941df694bdf25c8e50f847
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2020-06-01 15:00:20 +0200

    dolby vision: add import with profile option

--- scripts/dolby_vision.sh
+++ scripts/dolby_vision.sh
@@ -3,6 +3,9 @@ if [ $test_skip = 1 ] ; then
  return
 fi
 
+do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/counter.hvc:dv-profile=5 -new $TEMP_DIR/dolby_vision_profile.mp4" "import-set-profile"
+do_hash_test $TEMP_DIR/dolby_vision_profile.mp4 "profile"
+
 do_test "$MP4BOX -dash 2000 $MEDIA_DIR/dolby_vision/dolby_vision_cenc.ismv -out $TEMP_DIR/dolby_vision_cenc_dash.mpd" "dashing"
 do_hash_test $TEMP_DIR/dolby_vision_cenc_dashinit.mp4 "dash-seg"
 do_hash_test $TEMP_DIR/dolby_vision_cenc_dash.mpd "dash-mpd"
