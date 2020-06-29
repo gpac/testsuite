5cce7ec5d24176129d55dcefa654345c38a35959
jeanlf <jeanlf@gpac.io>
2020-06-29 14:07:46 +0200

    added test for forcesync

--- scripts/mp4box-gdr.sh
+++ scripts/mp4box-gdr.sh
@@ -4,9 +4,7 @@ return
 fi
 
 test_begin "mp4box-gdr"
-if [ $test_skip = 1 ] ; then
-return
-fi
+if [ $test_skip != 1 ] ; then
 
 mp4file="$TEMP_DIR/base.mp4"
 do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_GDR_320x180_160kbps.264 -new $mp4file" "add"
@@ -19,7 +17,19 @@ do_hash_test $mpdfile "dash-mpd"
 do_hash_test $TEMP_DIR/base_dashinit.mp4 "dash-rep"
 
 
+fi
 test_end
 
 
+
+test_begin "mp4box-forcesync"
+if [ $test_skip != 1 ] ; then
+
+mp4file="$TEMP_DIR/base.mp4"
+do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_openGOP_640x360_160kbps.264:forcesync -new $mp4file" "add"
+
+do_hash_test $mp4file "add"
+
+fi
+test_end
 
