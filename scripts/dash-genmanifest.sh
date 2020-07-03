589d9f252d5f809e7382e21f3728384ea3f0ee83
jeanlf <jeanlf@gpac.io>
2020-07-03 10:50:33 +0200

    added live profile tests for manifest only generation

--- scripts/dash-genmanifest.sh
+++ scripts/dash-genmanifest.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 test_begin "dash-gen-manifest"
-
- if [ $test_skip  = 1 ] ; then
-  return
- fi
+if [ $test_skip != 1 ] ; then
 
 do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_640x360_192kbps.264 -new $TEMP_DIR/source.mp4" "import"
 
@@ -20,5 +17,38 @@ do_test "$GPAC -i $TEMP_DIR/source_dashinit.mp4 -o $TEMP_DIR/gen-hls.m3u8:sigfra
 do_hash_test "$TEMP_DIR/gen-hls.m3u8" "gen-hls-master"
 do_hash_test "$TEMP_DIR/gen-hls_1.m3u8" "gen-hls-subpl"
 
+fi
+test_end
+
+test_begin "dash-gen-manifest-live"
+if [ $test_skip != 1 ] ; then
+
+do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_640x360_192kbps.264 -new $TEMP_DIR/source.mp4" "import"
+
+do_test "$MP4BOX -dash 2000 -profile live $TEMP_DIR/source.mp4 -out $TEMP_DIR/test.mpd" "dash"
+
+plist=$TEMP_DIR/input.txt
+echo "source_dashinit.mp4" > $plist
+echo "source_dash1.m4s" >> $plist
+echo "source_dash2.m4s" >> $plist
+echo "source_dash3.m4s" >> $plist
+echo "source_dash4.m4s" >> $plist
+echo "source_dash5.m4s" >> $plist
+echo "source_dash6.m4s" >> $plist
+echo "source_dash7.m4s" >> $plist
+echo "source_dash8.m4s" >> $plist
+echo "source_dash9.m4s" >> $plist
+
+do_test "$GPAC -i $plist -o $TEMP_DIR/gen-main.mpd:sigfrag" "gen-main"
+do_hash_test "$TEMP_DIR/gen-main.mpd" "gen-main"
+
+do_test "$GPAC -i $plist -o $TEMP_DIR/gen-live.mpd:sigfrag:profile=live:template=\$XInit=source_dashinit\$source_dash\$Number\$" "gen-live"
+do_hash_test "$TEMP_DIR/gen-live.mpd" "gen-live"
+
+do_test "$GPAC -i $plist -o $TEMP_DIR/gen-hls.m3u8:sigfrag" "gen-hls"
+do_hash_test "$TEMP_DIR/gen-hls.m3u8" "gen-hls-master"
+do_hash_test "$TEMP_DIR/gen-hls_1.m3u8" "gen-hls-subpl"
+
+fi
 test_end
 
