e3ad5cb19925f59afa02d59f8e9ece811e6b182b
jeanlf <jeanlf@gpac.io>
2020-10-09 16:40:53 +0200

    added test for multiple split of same sample in dasher

--- scripts/dash-subtitle.sh
+++ scripts/dash-subtitle.sh
@@ -10,10 +10,18 @@ do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/subtitle.srt -new $TEMP_DIR/fil
 do_test "$MP4BOX -dash 1500 $TEMP_DIR/file.mp4 -profile live -out $TEMP_DIR/file.mpd" "dash-subt"
 
 do_hash_test $TEMP_DIR/file.mpd "dash-subt-hash-mpd"
+do_hash_test $TEMP_DIR/file_dash3.m4s "dash-seg3"
 
 myinspect=$TEMP_DIR/inspect.txt
 do_test "$GPAC -i $TEMP_DIR/file.mpd inspect:allp:deep:interleave=false:log=$myinspect"
 do_hash_test $myinspect "inspect"
+
+#do another test with smaller segment duration, to exercice spliting of samples over 3 or more segments
+mv $TEMP_DIR/file.mp4 $TEMP_DIR/file2.mp4
+do_test "$MP4BOX -dash 500 $TEMP_DIR/file2.mp4 -profile live -out $TEMP_DIR/file2.mpd" "dash-subt2"
+
+do_hash_test $TEMP_DIR/file.mpd "dash-subt2-hash-mpd"
+do_hash_test $TEMP_DIR/file2_dash10.m4s "dash-seg10"
 
 
 test_end
