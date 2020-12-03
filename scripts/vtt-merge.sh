6dcb4bbc8ecddebe3199a49399c03e56b6d85452
jeanlf <jeanlf@gpac.io>
2020-12-03 12:26:54 +0100

    fixed wring vtt hashes

--- scripts/vtt-merge.sh
+++ scripts/vtt-merge.sh
@@ -1,14 +1,28 @@
 
- test_begin "vtt-merge"
- if [ $test_skip  = 1 ] ; then
-  return
- fi
+test_begin "vtt-merge"
+if [ $test_skip != 1 ] ; then
 
  mp4file=$TEMP_DIR/file.mp4
  vttfile=$TEMP_DIR/file.vtt
  do_test "$MP4BOX -add $MEDIA_DIR/webvtt/overlapping.vtt -new $mp4file" "vtt-to-mp4"
  do_test "$GPAC -i $mp4file -o $vttfile:merge" "vtt-merge"
  do_hash_test "$vttfile" "vtt-merge"
+fi
+test_end
 
- test_end
+
+test_begin "vtt-dump"
+if [ $test_skip != 1 ] ; then
+
+ mp4file=$TEMP_DIR/file.mp4
+ vttfile=$TEMP_DIR/file.vtt
+ srtfile=$TEMP_DIR/file.srt
+ do_test "$MP4BOX -add $MEDIA_DIR/webvtt/elephants-dream-chapters-en.vtt -new $mp4file" "vtt-to-mp4"
+ do_test "$MP4BOX -srt $mp4file -out $srtfile" "srt-extract"
+ do_hash_test "$srtfile" "srt-extract"
+ do_test "$MP4BOX -raw 1 $mp4file -out $vttfile" "vtt-extract"
+ do_hash_test "$vttfile" "vtt-extract"
+
+fi
+test_end
 
