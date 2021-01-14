df72dcd953790a46b56da92208b6c6d75b4ae6a1
jeanlf <jeanlf@gpac.io>
2021-01-14 12:17:02 +0100

    added tests for HLS full segment encryption

--- scripts/hls.sh
+++ scripts/hls.sh
@@ -45,3 +45,39 @@ test_end
 test_hls_crypt "cbcs" "cbcs_const.xml"
 test_hls_crypt "cbcs-roll" "cbcs_const_roll.xml"
 
+test_hls_crypt_seg()
+{
+
+test_begin "hls-cryp-seg-$1"
+if [ $test_skip != 1 ] ; then
+
+my_inspect=$TEMP_DIR/inspect_mp4.txt
+do_test "$GPAC $2 -i $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_1280x720_512kbps.264 -o $TEMP_DIR/file.m3u8:hlsdrm=$MEDIA_DIR/encryption/cbcs_const_roll2.xml" "cryp-mp4"
+do_hash_test "$TEMP_DIR/file.m3u8" "hls-master-mp4"
+do_hash_test "$TEMP_DIR/file_1.m3u8" "hls-child-mp4"
+do_hash_test "$TEMP_DIR/counter_30s_I25_baseline_1280x720_512kbps_dashinit.mp4" "init-seg-mp4"
+do_hash_test "$TEMP_DIR/counter_30s_I25_baseline_1280x720_512kbps_dash1.m4s" "first-seg-mp4"
+
+do_test "$GPAC $2 -i $TEMP_DIR/file.m3u8 inspect:deep:log=$my_inspect" "inspect-mp4"
+do_hash_test $my_inspect "inspect-mp4"
+
+
+my_inspect=$TEMP_DIR/inspect_ts.txt
+do_test "$GPAC $2 -i $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_1280x720_512kbps.264 -o $TEMP_DIR/file_ts.m3u8:hlsdrm=$MEDIA_DIR/encryption/cbcs_const_roll2.xml:muxtype=ts" "cryp-ts"
+do_hash_test "$TEMP_DIR/file_ts.m3u8" "hls-master-ts"
+do_hash_test "$TEMP_DIR/file_ts_1.m3u8" "hls-child-ts"
+do_hash_test "$TEMP_DIR/counter_30s_I25_baseline_1280x720_512kbps_dash1.ts" "first-seg-ts"
+
+do_test "$GPAC $2 -i $TEMP_DIR/file_ts.m3u8 inspect:deep:log=$my_inspect" "inspect-ts"
+do_hash_test $my_inspect "inspect-ts"
+
+
+fi
+test_end
+
+}
+
+test_hls_crypt_seg "block" ""
+test_hls_crypt_seg "fullfile" "--fullfile"
+
+
