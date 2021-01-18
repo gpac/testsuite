83e7e8be8e5ca0cef8d8e160f0060b93a3ca7268
jeanlf <jeanlf@gpac.io>
2021-01-18 12:23:06 +0100

    added tags from file test

--- scripts/mp4box-tags.sh
+++ scripts/mp4box-tags.sh
@@ -1,9 +1,7 @@
 
 
 test_begin "mp4box-tags"
-if [ "$test_skip" = 1 ] ; then
-return
-fi
+if [ "$test_skip" != 1 ] ; then
 
 mp4file="$TEMP_DIR/test.mp4"
 do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_audio.aac -new $mp4file -itags cover=$MEDIA_DIR/auxiliary_files/logo.png:genre=Game" "create"
@@ -15,5 +13,26 @@ covfile="$TEMP_DIR/test.png"
 do_test "$MP4BOX -dump-cover $mp4file -out $covfile" "dumpcover"
 do_hash_test $covfile "dumpcover"
 
+fi
+test_end
+
+
+test_begin "mp4box-tags-file"
+if [ "$test_skip" != 1 ] ; then
+
+tags=$TEMP_DIR/tags.txt
+echo "artist=GPAC" > $tags
+echo "lyrics=Some lyrics " >> $tags
+echo "  on  " >> $tags
+echo "multiple" >> $tags
+echo "" >> $tags
+echo "lines" >> $tags
+echo "cust=Custom Tag" >> $tags
+
+mp4file="$TEMP_DIR/test.mp4"
+do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_audio.aac -new $mp4file -itags $tags" "create"
+do_hash_test $mp4file "create"
+
+fi
 test_end
 
