2d30a3c85c4e11666eaf0111bbfdcbe3ec2fbbb2
jeanlf <jeanlf@gpac.io>
2021-02-03 11:46:41 +0100

    fixed hashes for ffenc edit lists and cmaf colr

--- scripts/dash-encode.sh
+++ scripts/dash-encode.sh
@@ -8,14 +8,14 @@ test_begin "dash-encode-single-v"
 if [ $test_skip = 0 ] ; then
 
 #load a video source, decode + resize it , encode it and and dash the result
-do_test "$GPAC -i $MEDIA_DIR/auxiliary_files/enst_video.h264:FID=1 ffsws:osize=512x512:SID=1 @ enc:c=avc:fintra=1 @ -o $TEMP_DIR/file.mpd:profile=live"
+do_test "$GPAC -i $MEDIA_DIR/auxiliary_files/enst_video.h264:FID=1 ffsws:osize=512x512:SID=1 @ enc:c=avc:fintra=1 @ -o $TEMP_DIR/file.mpd:profile=live" "dash"
 
 do_hash_test $TEMP_DIR/file.mpd "mpd"
 do_hash_test $TEMP_DIR/enst_video_dashinit.mp4 "init-v"
 
 #we don't want to test encoder result so hash the inspect timing, dts only: CTS and SAP might change due to reference frame selection by encoder
 myinspect=$TEMP_DIR/inspect.txt
-do_test "$GPAC -i $TEMP_DIR/file.mpd inspect:allp:interleave=false:fmt=%pn%-%dts%%lf%:log=$myinspect"
+do_test "$GPAC -i $TEMP_DIR/file.mpd inspect:allp:interleave=false:fmt=%pn%-%dts%%lf%:log=$myinspect" "inspect"
 do_hash_test $myinspect "inspect"
 
 fi
@@ -26,14 +26,14 @@ test_begin "dash-encode-single-a"
 if [ $test_skip = 0 ] ; then
 
 #load an audio source, decode it , encode it and dash the result
-do_test "$GPAC -i $MEDIA_DIR/auxiliary_files/count_english.mp3 @ enc:c=aac @ -o $TEMP_DIR/file.mpd:profile=live"
+do_test "$GPAC -i $MEDIA_DIR/auxiliary_files/count_english.mp3 @ enc:c=aac @ -o $TEMP_DIR/file.mpd:profile=live" "dash"
 
 do_hash_test $TEMP_DIR/file.mpd "mpd"
 do_hash_test $TEMP_DIR/count_english_dashinit.mp4 "init-a"
 
 #we don't want to test encoder result so hash the inspect timing and sap
 myinspect=$TEMP_DIR/inspect.txt
-do_test "$GPAC -i $TEMP_DIR/file.mpd inspect:allp:interleave=false:fmt=%pn%-%dts%-%cts%-%sap%%lf%:log=$myinspect"
+do_test "$GPAC -i $TEMP_DIR/file.mpd inspect:allp:interleave=false:fmt=%pn%-%dts%-%cts%-%sap%%lf%:log=$myinspect" "inspect"
 do_hash_test $myinspect "inspect"
 
 fi
@@ -44,7 +44,7 @@ test_begin "dash-encode-vv"
 if [ $test_skip = 0 ] ; then
 
 #load a source, decode it , resize+encode in two resolutions and dash the result
-do_test "$GPAC -i $MEDIA_DIR/auxiliary_files/enst_video.h264:FID=1 ffsws:osize=512x512:SID=1 @ enc:c=avc:fintra=1:FID=EV1 ffsws:osize=256x256:SID=1 @ enc:c=avc:fintra=1:FID=EV2 -o $TEMP_DIR/file.mpd:profile=live:SID=EV1,EV2 -graph"
+do_test "$GPAC -i $MEDIA_DIR/auxiliary_files/enst_video.h264:FID=1 ffsws:osize=512x512:SID=1 @ enc:c=avc:fintra=1:FID=EV1 ffsws:osize=256x256:SID=1 @ enc:c=avc:fintra=1:FID=EV2 -o $TEMP_DIR/file.mpd:profile=live:SID=EV1,EV2 -graph" "dash"
 
 do_hash_test $TEMP_DIR/file.mpd "mpd"
 do_hash_test $TEMP_DIR/enst_video_dashinit_rep1.mp4 "init-v1"
@@ -52,7 +52,7 @@ do_hash_test $TEMP_DIR/enst_video_dashinit_rep2.mp4 "init-v2"
 
 #we don't want to test encoder result so hash the inspect timing, dts only: CTS and SAP might change due to reference frame selection by encoder
 myinspect=$TEMP_DIR/inspect.txt
-do_test "$GPAC -i $TEMP_DIR/file.mpd  --auto_switch=1 inspect:allp:interleave=false:fmt=%pn%-%dts%%lf%:log=$myinspect"
+do_test "$GPAC -i $TEMP_DIR/file.mpd  --auto_switch=1 inspect:allp:interleave=false:fmt=%pn%-%dts%%lf%:log=$myinspect" "inspect"
 do_hash_test $myinspect "inspect"
 
 fi
@@ -66,7 +66,7 @@ if [ $test_skip = 0 ] ; then
 #load a video MP3G-4part2 source, decode it , resize+encode in AVC in two resolutions
 #load a an audio MP3 source, decode + encode in AAC
 #dash the result
-do_test "$GPAC -i $MEDIA_DIR/auxiliary_files/count_video.cmp:FID=1 ffsws:osize=512x512:SID=1 @ enc:c=avc:fintra=1:FID=EV1 ffsws:osize=256x256:SID=1 @ enc:c=avc:fintra=1:FID=EV2 -i $MEDIA_DIR/auxiliary_files/count_english.mp3:FID=2  @ enc:c=aac:FID=EA -o $TEMP_DIR/file.mpd:profile=live:SID=EV1,EV2,EA -graph -blacklist=vtbdec"
+do_test "$GPAC -i $MEDIA_DIR/auxiliary_files/count_video.cmp:FID=1 ffsws:osize=512x512:SID=1 @ enc:c=avc:fintra=1:FID=EV1 ffsws:osize=256x256:SID=1 @ enc:c=avc:fintra=1:FID=EV2 -i $MEDIA_DIR/auxiliary_files/count_english.mp3:FID=2  @ enc:c=aac:FID=EA -o $TEMP_DIR/file.mpd:profile=live:SID=EV1,EV2,EA -graph -blacklist=vtbdec" "dash"
 
 do_hash_test $TEMP_DIR/file.mpd "mpd"
 do_hash_test $TEMP_DIR/count_video_dashinit_rep1.mp4 "init-v1"
@@ -75,7 +75,7 @@ do_hash_test $TEMP_DIR/count_english_dashinit.mp4 "init-a"
 
 #we don't want to test encoder result so hash the inspect timing, dts only: CTS and SAP might change due to reference frame selection by encoder
 myinspect=$TEMP_DIR/inspect.txt
-do_test "$GPAC -i $TEMP_DIR/file.mpd --auto_switch=1 inspect:allp:interleave=false:fmt=%pn%-%dts%%lf%:log=$myinspect"
+do_test "$GPAC -i $TEMP_DIR/file.mpd --auto_switch=1 inspect:allp:interleave=false:fmt=%pn%-%dts%%lf%:log=$myinspect" "inspect"
 do_hash_test $myinspect "inspect"
 
 fi
@@ -93,7 +93,7 @@ do_hash_test $TEMP_DIR/source_dashinit.mp4 "init-seg"
 
 #we don't want to test encoder result so hash the inspect timing, dts only: CTS and SAP might change due to reference frame selection by encoder
 myinspect=$TEMP_DIR/inspect.txt
-do_test "$GPAC -i $TEMP_DIR/file.mpd inspect:allp:interleave=false:fmt=%pn%-%dts%%lf%:log=$myinspect"
+do_test "$GPAC -i $TEMP_DIR/file.mpd inspect:allp:interleave=false:fmt=%pn%-%dts%%lf%:log=$myinspect" "inspect"
 do_hash_test $myinspect "inspect"
 
 fi
@@ -113,7 +113,7 @@ do_hash_test $TEMP_DIR/source_dash_track2_init.mp4 "init-seg-aud"
 
 #we don't want to test encoder result so hash the inspect timing, dts only: CTS and SAP might change due to reference frame selection by encoder
 myinspect=$TEMP_DIR/inspect.txt
-do_test "$GPAC -i $TEMP_DIR/file.mpd inspect:allp:interleave=false:fmt=%pn%-%dts%%lf%:log=$myinspect"
+do_test "$GPAC -i $TEMP_DIR/file.mpd inspect:allp:interleave=false:fmt=%pn%-%dts%%lf%:log=$myinspect" "inspect"
 do_hash_test $myinspect "inspect"
 
 fi
