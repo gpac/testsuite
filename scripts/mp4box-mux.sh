0e4a87e50dad67206fdad0d5fc020d0f0a052c17
jeanlf <jeanlf@gpac.io>
2021-02-08 11:26:40 +0100

    added -mux tests

---
+++ scripts/mp4box-mux.sh
@@ -0,0 +1,35 @@
+
+
+
+test_begin "mp4box-mux"
+if [ "$test_skip" = 1 ] ; then
+return
+fi
+
+mp4file="$TEMP_DIR/test.mp4"
+do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_video.h264 -add $MEDIA_DIR/auxiliary_files/enst_audio.aac -new $mp4file" "add"
+do_hash_test $mp4file "add"
+
+tsfile="$TEMP_DIR/test.ts"
+do_test "$MP4BOX -mux $tsfile:pcr_init=0:pes_pack=none $mp4file" "mux-ts"
+do_hash_test $tsfile "mux-ts"
+
+
+do_test "$MP4BOX -raw video $tsfile" "tsdump-video"
+do_hash_test $TEMP_DIR/test.264 "tsdump-video"
+
+do_test "$MP4BOX -raw 101 $tsfile" "tsdump-audio"
+do_hash_test $TEMP_DIR/test.aac "tsdump-audio"
+
+#check if we have libavformat support, if so test ts->mkv and mp4->mkv
+ffmx=`$GPAC -h ffmx 2>/dev/null | grep ffmx`
+if [ -n "$ffmx" ] ; then
+#we don't hash mkv, not bitexact depending on platforms - this is checked in ffmx.sh
+do_test "$MP4BOX -mux $TEMP_DIR/test1.mkv $tsfile" "ts-mkv"
+
+do_test "$MP4BOX -mux $TEMP_DIR/test2.mkv $mp4file" "mp4-mkv"
+
+fi
+
+test_end
+
