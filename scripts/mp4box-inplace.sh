257f98313ddac0659e5d5267cda764565823d516
jeanlf <jeanlf@gpac.io>
2021-03-23 16:40:22 +0100

    fixed and new hashes for HEIF image to track references

--- scripts/mp4box-inplace.sh
+++ scripts/mp4box-inplace.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-test_rescale()
+test_inplace()
 {
 test_begin "mp4box-$1"
 
@@ -38,8 +38,41 @@ test_end
 
 }
 
-test_rescale "inplace-av-inter" "-add $MEDIA_DIR/auxiliary_files/enst_video.h264 -add $MEDIA_DIR/auxiliary_files/enst_audio.aac -inter 500" 0
-test_rescale "inplace-av-flat" "-add $MEDIA_DIR/auxiliary_files/enst_video.h264 -add $MEDIA_DIR/auxiliary_files/enst_audio.aac -flat" 0
-test_rescale "inplace-iff" "-add-image $MEDIA_DIR/auxiliary_files/enst_video.h264" 1
-test_rescale "inplace-dual-inter" "-add-image $MEDIA_DIR/auxiliary_files/enst_video.h264 -add $MEDIA_DIR/auxiliary_files/enst_video.h264 -inter 500" 0
-test_rescale "inplace-dual-flat" "-add $MEDIA_DIR/auxiliary_files/enst_video.h264 -add-image :ref -flat" 0
+test_inplace "inplace-av-inter" "-add $MEDIA_DIR/auxiliary_files/enst_video.h264 -add $MEDIA_DIR/auxiliary_files/enst_audio.aac -inter 500" 0
+test_inplace "inplace-av-flat" "-add $MEDIA_DIR/auxiliary_files/enst_video.h264 -add $MEDIA_DIR/auxiliary_files/enst_audio.aac -flat" 0
+test_inplace "inplace-iff" "-add-image $MEDIA_DIR/auxiliary_files/enst_video.h264" 1
+test_inplace "inplace-dual-inter" "-add-image $MEDIA_DIR/auxiliary_files/enst_video.h264 -add $MEDIA_DIR/auxiliary_files/enst_video.h264 -inter 500" 0
+test_inplace "inplace-dual-flat" "-add $MEDIA_DIR/auxiliary_files/enst_video.h264 -add-image :ref -flat" 0
+
+
+test_inplace_meta_ref()
+{
+test_begin "mp4box-inplace-iff-ref"
+
+ if [ $test_skip  = 1 ] ; then
+  return
+ fi
+
+#test inplace editing without padding
+src=$TEMP_DIR/src.heic
+do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_1280x720_512kbps.264 -new $src" "input"
+do_hash_test $src "input"
+
+#test inplace editing
+dst=$TEMP_DIR/img_ref1.heic
+cp $src $dst
+
+do_test "$MP4BOX -add-image ref:samp=1 $dst" "add-ref-samp1"
+do_hash_test $src "add-ref-samp1"
+
+dst2=$TEMP_DIR/img_ref2.heic
+cp $dst $dst2
+do_test "$MP4BOX -add-image ref:samp=251 $dst2" "add-ref-samp2"
+do_hash_test $src "add-ref-samp2"
+
+test_end
+
+}
+
+test_inplace_meta_ref
+
