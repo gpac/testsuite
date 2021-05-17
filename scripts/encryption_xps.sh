fb48be4b9044357ef8276986c6eca46bca13c653
jeanlf <jeanlf@gpac.io>
2021-05-17 14:44:47 +0200

    added tests for inband pps encryption

--- scripts/encryption_xps.sh
+++ scripts/encryption_xps.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-#test import of encrypted files, whether flat or fragmented. 
-test_begin "encryption-xps"
+#test import of encrypted files, whether flat or fragmented.
+encrypt_xps()
+{
+test_begin "encryption-$1"
 if [ $test_skip  = 1 ] ; then
  return
 fi
 
 mp4file="$TEMP_DIR/src.mp4"
 
-$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_640x360_192kbps.264:dur=10:xps_inband -new $mp4file 2> /dev/null
+$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_640x360_192kbps.264:dur=10:xps_inband$2 -new $mp4file 2> /dev/null
 
 crypt="$TEMP_DIR/ctr_cryp.mp4"
 dcrypt="$TEMP_DIR/ctr_decryp.mp4"
@@ -30,3 +32,8 @@ do_hash_test $dcrypt "decrypt-cbcs"
 
 
 test_end
+}
+
+
+encrypt_xps "xps" ""
+encrypt_xps "pps" ":dopt:pps_inband"
