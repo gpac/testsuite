75c774ec18d375744ad3f20c4814aa8e8e4a1c58
jeanlf <jeanlf@gpac.io>
2021-07-12 10:42:50 +0200

    add test for multi-stsd fragemented and protected file decryption

--- scripts/encryption_exotic.sh
+++ scripts/encryption_exotic.sh
@@ -64,3 +64,33 @@ test_end
 test_encrypt_mkey "base" "mkey_base.xml"
 test_encrypt_mkey "roll" "mkey_roll.xml"
 test_encrypt_mkey "subs" "mkey_subs.xml"
+
+
+
+#test encryption with multiple sample desc
+test_encrypt_multi_stsd()
+{
+test_begin "encryption-multi-stsd"
+if [ $test_skip  = 1 ] ; then
+ return
+fi
+
+cfile=$TEMP_DIR/crypt.mp4
+dfile=$TEMP_DIR/decrypt.mp4
+mfile=$TEMP_DIR/vod.mpd
+cffile=$TEMP_DIR/crypt_dashinit.mp4
+
+do_test "$GPAC -i $src cecrypt:cfile=$MEDIA_DIR/encryption/ctr_clear_stsd_after.xml @ -o $cfile" "encrypt"
+do_hash_test $cfile "encrypt"
+
+do_test "$MP4BOX -dash 1000 -profile onDemand -out $mfile $cfile" "dash"
+do_hash_test $cffile "dash"
+
+
+do_test "$MP4BOX -decrypt $cffile -out $dfile" "decrypt"
+do_hash_test $dfile "decrypt"
+
+test_end
+}
+
+test_encrypt_multi_stsd
