6cbbfdfa6f7152521c2d5419b4c41a075b05a5cd
jeanlf <jeanlf@gpac.io>
2021-11-26 16:02:19 +0100

    added vui change tests for hevc and vvc

--- scripts/mp4box-vui.sh
+++ scripts/mp4box-vui.sh
@@ -18,10 +18,16 @@ mp4file="$TEMP_DIR/fullrange_off.mp4"
 do_test "$MP4BOX -add $1:fullrange=off -new $mp4file" "add-fr-off"
 do_hash_test $mp4file "add-fr-off"
 
+mp4file="$TEMP_DIR/par_16_9.mp4"
+do_test "$MP4BOX -add $1:par=w16:9 -new $mp4file" "set-sar"
+do_hash_test $mp4file "set-sar"
 
 test_end
 }
 
 test_vui $MEDIA_DIR/auxiliary_files/enst_video.h264
+test_vui $MEDIA_DIR/auxiliary_files/counter.hvc
+test_vui $EXTERNAL_MEDIA_DIR/counter/counter_30s_1280x720p_I25_closedGOP_512kpbs.vvc
+
 
 
