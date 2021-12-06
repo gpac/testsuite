992fd9374bc915e7a26d14aa85c22025a4220631
jeanlf <jeanlf@gpac.io>
2021-12-06 09:29:22 +0100

    added subtitle only dash test

--- scripts/dash-subtitle.sh
+++ scripts/dash-subtitle.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 test_begin "dash-subt"
-if [ $test_skip  = 1 ] ; then
-return
-fi
+if [ $test_skip != 1 ] ; then
 
 do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/subtitle.srt -new $TEMP_DIR/file.mp4" "dash-input-preparation"
 
@@ -23,5 +21,17 @@ do_test "$MP4BOX -dash 500 $TEMP_DIR/file2.mp4 -profile live -out $TEMP_DIR/file
 do_hash_test $TEMP_DIR/file.mpd "dash-subt2-hash-mpd"
 do_hash_test $TEMP_DIR/file2_dash10.m4s "dash-seg10"
 
+fi
+test_end
 
+
+test_begin "dash-subtitles"
+if [ $test_skip != 1 ] ; then
+
+do_test "$GPAC -i $MEDIA_DIR/auxiliary_files/subtitle.srt:#Language=en  -i $MEDIA_DIR/auxiliary_files/subtitle_fr.srt:#Language=fr -o $TEMP_DIR/live.mpd:stl" "dash-subs"
+do_hash_test $TEMP_DIR/live.mpd "dash-subs-mpd"
+do_hash_test $TEMP_DIR/subtitle_dash4000.m4s "seg4-en"
+do_hash_test $TEMP_DIR/subtitle_fr_dash4000.m4s "seg4-fr"
+
+fi
 test_end
