efaeaea9edca8eed9d9a7f362655037520f4d3bb
jeanlf <jeanlf@gpac.io>
2022-01-06 16:54:54 +0100

    updated dovi test

--- scripts/dolby_vision.sh
+++ scripts/dolby_vision.sh
@@ -6,7 +6,7 @@ fi
 do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/counter.hvc:dv-profile=5 -new $TEMP_DIR/dolby_vision_profile.mp4" "import-set-profile"
 do_hash_test $TEMP_DIR/dolby_vision_profile.mp4 "profile"
 
-do_test "$MP4BOX -add $TEMP_DIR/dolby_vision_profile.mp4:dv-profile=0 -new $TEMP_DIR/dolby_vision_profile_rem.mp4" "import-rem-profile"
+do_test "$MP4BOX -add $TEMP_DIR/dolby_vision_profile.mp4:dv-profile=none -new $TEMP_DIR/dolby_vision_profile_rem.mp4" "import-rem-profile"
 do_hash_test $TEMP_DIR/dolby_vision_profile_rem.mp4 "profile-rem"
 
 do_test "$MP4BOX -dash 2000 $MEDIA_DIR/dolby_vision/dolby_vision_cenc.ismv -out $TEMP_DIR/dolby_vision_cenc_dash.mpd" "dashing"
