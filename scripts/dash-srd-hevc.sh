444e9619d70fd1f561d3924874689f3d6e65dfdb
jeanlf <jeanlf@gpac.io>
2023-09-08 15:50:26 +0200

    added tests for dash srd hevc merging

--- scripts/dash-srd-hevc.sh
+++ scripts/dash-srd-hevc.sh
@@ -60,3 +60,33 @@ test_end
 fi
 
 
+
+#test dual resolution spliting
+test_begin "dash-srd-hevc-dual-split"
+if [ $test_skip != 1 ] ; then
+
+do_test "$GPAC -i $EXTERNAL_MEDIA_DIR/counter/counter_1280_720_I_25_tiled_qp20.hevc hevcsplit -o $TEMP_DIR/large.mp4" "dash-input-preparation1"
+do_hash_test $TEMP_DIR/large.mp4 "split-tiles1"
+do_test "$GPAC -i $EXTERNAL_MEDIA_DIR/counter/counter_640_360_I_25_tiled_qp30.hevc hevcsplit -o $TEMP_DIR/small.mp4" "dash-input-preparation2"
+do_hash_test $TEMP_DIR/small.mp4 "split-tiles2"
+
+do_test "$MP4BOX -dash 1000 -profile live $TEMP_DIR/large.mp4 $TEMP_DIR/small.mp4 -out $TEMP_DIR/file.mpd" "basic-dash"
+
+do_hash_test $TEMP_DIR/file.mpd "mpd"
+
+myinspect=$TEMP_DIR/inspect_tiles.txt
+do_test "$GPAC -i $TEMP_DIR/file.mpd inspect:allp:deep:interleave=false:log=$myinspect" "inspect-tiles"
+do_hash_test $myinspect "inspect-tiles"
+
+myinspect=$TEMP_DIR/inspect_agg.txt
+do_test "$GPAC -i $TEMP_DIR/file.mpd --algo=none --auto_switch=1 hevcmerge inspect:allp:deep:interleave=false:log=$myinspect" "inspect-merge"
+do_hash_test $myinspect "inspect-merge"
+
+#also do a playback test for coverage of SRDmap - hevcmerge filter is automatically loaded when using compositor (including with -mp4c, -gui)
+do_test "$GPAC --noaudio -blacklist=vtbdec,nvdec -mp4c $TEMP_DIR/file.mpd#VR -runfor=500" "play-merge-vr"
+do_test "$GPAC --noaudio -blacklist=vtbdec,nvdec -mp4c $TEMP_DIR/file.mpd -runfor=500" "play-merge"
+
+test_end
+fi
+
+
