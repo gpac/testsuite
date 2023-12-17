6ef009bc2808be89b6b3ba73e2843ba69cae7671
jeanlf <jeanlf@gpac.io>
2023-12-17 12:36:15 +0100

    added prober and anlyze oss fuzz test

---
+++ oss-fuzzers/fuzz_probe_analyze.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include <gpac/filters.h>
+#include <gpac/constants.h>
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    char filename[256];
+    GF_Err e;
+    sprintf(filename, "/tmp/libfuzzer.%d", getpid());
+
+    FILE *fp = fopen(filename, "wb");
+    if (!fp) return 0;
+    fwrite(data, size, 1, fp);
+    fclose(fp);
+
+    GF_FilterSesion *fs = gf_fs_new_defaults(0);
+    GF_Filter *src = gf_fs_load_source(fs, filename, NULL, NULL, &e);
+    GF_Filter *insp = gf_fs_load_filter(fs, "inspect:deep:analyze=bs", &e);
+    gf_fs_run(fs);
+	gf_fs_del(fs);
+
+    unlink(filename);
+    return 0;
+}
