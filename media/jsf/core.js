5c91c8999a41d185dae208b90fb288d5e8f88ef9
jeanlf <jeanlf@gpac.io>
2024-04-13 09:40:27 +0200

    avoid possible bug in JSF core test

--- media/jsf/core.js
+++ media/jsf/core.js
@@ -120,7 +120,7 @@ sys.color_component('black', 3);
 sys.rect_intersect({x: 0, y: 0, w: 20, h: 20}, {x: 10, y: 10, w: 20, h: 20}  );
 
 try {
-	sys.load_script('test.js');
+	sys.load_script('__null_test.js');
 } catch (e) {
 
 }
