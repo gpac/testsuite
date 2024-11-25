dd384051dad45a05474666a9af40964af68f366b
Felicia Lim <flim@google.com>
2024-11-25 15:35:32 -0800

    added iamf reframer test and updated mp4box-mux test
    audio_opus.iamf is taken from test_000305.iamf in
    https://github.com/AOMediaCodec/libiamf/tree/main/tests

--- scripts/mp4box-mux.sh
+++ scripts/mp4box-mux.sh
@@ -36,7 +36,7 @@ do_test "$MP4BOX -mux $TEMP_DIR/test2.mkv $mp4file" "mp4-mkv"
 fi
 
 iamf_mp4file="$TEMP_DIR/test-iamf.mp4"
-do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/iamf_video.mp4 -add $MEDIA_DIR/auxiliary_files/iamf_audio.mp4 -new $iamf_mp4file" "add-iamf"
+do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/blank_video_5s.mp4 -add $MEDIA_DIR/auxiliary_files/audio_opus.iamf -new $iamf_mp4file" "add-iamf"
 do_hash_test $iamf_mp4file "add-iamf"
 
 test_end
