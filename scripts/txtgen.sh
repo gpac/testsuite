df6177befea5dce1bc9d89f364dcad4647eee0de
jeanlf <jeanlf@gpac.io>
2025-04-07 09:09:26 +0200

    fixed script for gpac/gpac@b1710c58

--- scripts/txtgen.sh
+++ scripts/txtgen.sh
@@ -4,16 +4,16 @@ if [ $test_skip = 1 ]; then
 fi
 
 dump=$TEMP_DIR/dump.vtt
-do_test "$GPAC txtgen:w:udur=1/5:dur=5:rollup=2:!rt tx3g2vtt -o $dump" "word-rollup"
+do_test "$GPAC txtgen:w:fdur=1/5:dur=5:rollup=2:!rt tx3g2vtt -o $dump" "word-rollup"
 do_hash_test $dump "word-rollup"
 
-do_test "$GPAC txtgen:l:udur=1/5:dur=5:rollup=2:!rt tx3g2vtt -o $dump" "line-rollup"
+do_test "$GPAC txtgen:l:fdur=1/5:dur=5:rollup=2:!rt tx3g2vtt -o $dump" "line-rollup"
 do_hash_test $dump "line-rollup"
 
-do_test "$GPAC txtgen:udur=1/5:dur=5:lmax=16:!rt tx3g2vtt -o $dump" "lmax"
+do_test "$GPAC txtgen:fdur=1/5:dur=5:lmax=16:!rt tx3g2vtt -o $dump" "lmax"
 do_hash_test $dump "lmax"
 
-do_test "$GPAC txtgen:udur=1:dur=5:!rt tx3g2vtt -o $dump" "udur"
+do_test "$GPAC txtgen:fdur=1:dur=5:!rt tx3g2vtt -o $dump" "udur"
 do_hash_test $dump "udur"
 
 test_end
