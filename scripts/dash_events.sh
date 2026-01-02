0335f56e92f5ca4d727c1703c1a7102fee8e9188
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2026-01-02 10:42:53 -0400

    dash inband event: add missing test (before building on top of it)

---
+++ scripts/dash_events.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+dash_event_test()
+{
+    test_begin $1
+    if [ $test_skip  = 1 ] ; then
+        return
+    fi
+
+    do_test "$GPAC -i $mp4file -o $TEMP_DIR/file.mpd:inband_event" "dash"
+    do_hash_test $TEMP_DIR/file.mpd "$1-hash-mpd"
+
+    test_end
+}
+
+mp4file=$TEMP_DIR/file.mp4
+$GPAC -i $MEDIA_DIR/auxiliary_files/enst_audio.aac -o $mp4file 2> /dev/null
+
+dash_event_test "inband-events"
