a3019f4b7e734680427bf43382b6698a2dd58ddd
Jean Le Feuvre <jeanlf@gpac.io>
2026-04-22 18:20:11 +0200

    updated GHI tests

--- scripts/ghi.sh
+++ scripts/ghi.sh
@@ -10,43 +10,45 @@ return
 fi
 
 idx_file=$TEMP_DIR/idx.$2
-src_file=$TEMP_DIR/src.$3
+src_file=$TEMP_DIR/src.$4
+ghiopt="$3:segdur=2"
 
-do_test "$GPAC -i $MEDIA_DIR/auxiliary_files/counter.hvc -i $MEDIA_DIR/auxiliary_files/count_english.mp3 -o $src_file$4"  "gen-src"
+SN=$9
+
+do_test "$GPAC -i $MEDIA_DIR/auxiliary_files/counter.hvc -i $MEDIA_DIR/auxiliary_files/count_english.mp3 -o $src_file$5"  "gen-src"
 do_hash_test "$src_file" "gen-src"
 
-do_test "$GPAC -i $src_file$5 -o $idx_file:segdur=2"  "idx"
+do_test "$GPAC -i $src_file$6 -o $idx_file$ghiopt"  "idx"
 do_hash_test "$idx_file" "idx"
 
 #generate manifest + init segments
 dst_mpd=$TEMP_DIR/vod.mpd
-do_test "$GPAC -i $idx_file:gm=all$6 -o $dst_mpd$7"  "mpd"
+do_test "$GPAC -i $idx_file:gm=all$6 -o $dst_mpd$8"  "mpd"
 do_hash_test "$dst_mpd" "mpd"
 
-if [ "$7" = "" ] ; then
+if [ "$8" = "" ] ; then
 do_hash_test "$TEMP_DIR/1-init.mp4" "init"
 fi
 
 #generate master manifest only
 dst_hls=$TEMP_DIR/vod.m3u8
-do_test "$GPAC -i $idx_file:gm=main$6 -o $dst_hls$7"  "hls"
+do_test "$GPAC -i $idx_file:gm=main$7 -o $dst_hls$8"  "hls"
 do_hash_test "$dst_hls" "hls"
 
 #generate child manifest only
 child_hls=$TEMP_DIR/vod_r1.m3u8
-do_test "$GPAC -i $idx_file:gm=child:rep=1$6:out=$child_hls -o $dst_hls$7"  "hls-child"
+do_test "$GPAC -i $idx_file:gm=child:rep=1$7:out=$child_hls -o $dst_hls$8"  "hls-child"
 do_hash_test "$child_hls" "hls-child"
 
 if [ "$7" = "" ] ; then
 #generate rep1 init only
 rep_init=$TEMP_DIR/vod_r1.mp4
-do_test "$GPAC -i $idx_file:gm=init:rep=1$6:out=$rep_init -o $dst_hls$7"  "hls-init"
+do_test "$GPAC -i $idx_file:gm=init:rep=1$7:out=$rep_init -o $dst_hls$8"  "hls-init"
 do_hash_test "$rep_init" "hls-init"
 fi
 
-#generate seg 4
-rep_seg=$TEMP_DIR/vod_r1_4.m4s
-do_test "$GPAC -i $idx_file:rep=1$6:sn=4:out=$rep_seg -o $dst_hls$7"  "seg-gen"
+rep_seg=$TEMP_DIR/vod_r1_$SN.m4s
+do_test "$GPAC -i $idx_file:rep=1$7:sn=$SN:out=$rep_seg -o $dst_hls$8"  "seg-gen"
 do_hash_test "$rep_seg" "seg-gen"
 
 #do_test "$GPAC --initseg=$TEMP_DIR/vod_r1.mp4 -i $TEMP_DIR/vod_r1_4.m4s vout" "play"
@@ -55,13 +57,22 @@ test_end
 
 }
 
-test_ghi "ghi-mp4" "ghi" "mp4" "" ":nodata=yes" "" ""
+#test binary indexing from mp4
+test_ghi "ghi-mp4" "ghi" "" "mp4" "" ":nodata=yes" "" "" 4
+#test binary indexing from ts
+test_ghi "ghi-ts-mux" "ghi" "" "ts" "" "" ":mux=2@1" "" 4
 
-test_ghi "ghix-fmp4" "ghix" "mp4" ":frag" ":sigfrag:nodata=yes" "" ""
+#test binary indexing from mp4 with TS segment generation
+test_ghi "ghi-mux-ts" "ghi" "" "mp4" "" "" ":mux=2@1" ":muxtype=ts" 4
 
-test_ghi "ghi-ts-mux" "ghi" "ts" "" ":sigfrag" ":mux=2@1" ""
+#test text indexing from fragmented mp4 with 1s fragment duration (less than segdur), no fragment offsets
+test_ghi "ghix-fmp4" "ghix" "" "mp4" ":frag" ":sfx:nodata=yes" "" "" 4
 
-test_ghi "ghi-mux-ts" "ghi" "mp4" "" ":sigfrag" ":mux=2@1" ":muxtype=ts"
+#test text indexing from fragmented mp4 with 5s fragment duration, fragment offsets
+test_ghi "ghix-fmp4-nfrags" "ghix" ":sigfo" "mp4" ":frag:cdur=6" ":nodata=yes" "" "" 4
+
+#test text indexing from fragmented mp4 with 5s fragment duration, using source fragmentation cues
+test_ghi "ghix-fmp4-sigfrag" "ghix" ":sigfrag" "mp4" ":frag:cdur=6" ":nodata=yes" "" "" 2
 
 
 #test on the fly encryption
